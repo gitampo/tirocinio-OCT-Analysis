aa0db33b98a20593e3472ef13bd6aa23832444ff
Gabriele Mantega <gabrielemantega@gmail.com>
2025-07-07 11:05:32 +0200

    Alcuni file per mockup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from classes.OCTAnalysisApp import *
     
-# on script run as main
+# script eseguito come main
 if __name__ == '__main__':
     app = OCTAnalysisApp()
     app.mainloop()

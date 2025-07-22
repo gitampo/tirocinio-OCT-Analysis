e66777833a10122d32a9a9d1a1eb546ff93580a9
Gabriele Mantega <gabrielemantega@gmail.com>
2025-07-22 10:45:49 +0200

    immagini per mockup

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from database import db_manager
 
 # quando lo script eseguito come main
 if __name__ == '__main__':
+    
     # connessione al database e avvio applicazione
     with sqlite3.connect(PT_database) as db_connection:
         db_manager.db_connection = db_connection    # imposta la connessione del db_manager 

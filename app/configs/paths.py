d5f12242f5bd9a0a2bd828df0c089aa3c94c3312
gitampo <tamponi.fra@libero.it>
2026-04-29 14:12:10 +0200

    fix kaggle

--- app/configs/paths.py
+++ app/configs/paths.py
@@ -13,6 +13,6 @@ PT_log_dir = 'logs/'
 # DATASET (KAGGLE + LOCAL)
 # -------------------------
 if os.path.exists("/kaggle/input"):
-    PT_datasets_dir = "/kaggle/input/datasets/orvile" 
+    PT_datasets_dir = "/kaggle/input/datasets" 
 else:
     PT_datasets_dir = 'deeplearning/data/datasets/'

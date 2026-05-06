c13c640688aca328dcba89c7940fce62d7b45fdf
gitampo <tamponi.fra@libero.it>
2026-05-06 23:43:16 +0200

    fix kaggle dataset nuovo

--- app/deeplearning/datasets/OCTDL.py
+++ app/deeplearning/datasets/OCTDL.py
@@ -1,11 +1,9 @@
-import pandas as pd
 from pathlib import Path
 from configs.paths import PT_datasets_dir
 
 DATASET_NAME = 'OCT2017 '
-LABELS_CSV = "OCTDL_labels.csv"
 
-labels = ['AMD','DME','ERM','NO','RAO','RVO','VID'] # l'ordine è importante
+labels = ['CNV', 'DME', 'DRUSEN', 'NORMAL']
 
 def id2label(id):
     return labels[id]
@@ -14,12 +12,6 @@ def label2id(label):
     return labels.index(label)
 
 def get_patient_id(image_name):
-
-    dataset_root = Path(PT_datasets_dir)        # .../OCTDL
-    df = pd.read_csv(dataset_root / LABELS_CSV)[["file_name", "patient_id"]]
-
-    patient_id = int(
-        df[df["file_name"] == image_name]["patient_id"].values[0]
-    )
-
-    return patient_id
+    # nel dataset Kaggle non esistono patient_id
+    # usiamo il nome immagine come identificativo univoco
+    return image_name

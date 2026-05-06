312e5614de3cb516116379710bfe9078a721c2ef
gitampo <tamponi.fra@libero.it>
2026-05-06 20:29:44 +0200

    fix kaggle

--- app/deeplearning/datasets/OCTDL.py
+++ app/deeplearning/datasets/OCTDL.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 from configs.paths import PT_datasets_dir
 
-DATASET_NAME = 'OCTDL2017 '
+DATASET_NAME = 'OCT2017 '
 LABELS_CSV = "OCTDL_labels.csv"
 
 labels = ['AMD','DME','ERM','NO','RAO','RVO','VID'] # l'ordine è importante

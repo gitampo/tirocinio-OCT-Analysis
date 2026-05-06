866e96726876327d0e9513201fa57d33eadd4bba
gitampo <tamponi.fra@libero.it>
2026-05-06 17:36:15 +0200

    fix kaggle nuovo dataset

--- app/configs/paths.py
+++ app/configs/paths.py
@@ -13,6 +13,6 @@ PT_log_dir = 'logs/'
 # DATASET (KAGGLE + LOCAL)
 # -------------------------
 if os.path.exists("/kaggle/input"):
-    PT_datasets_dir = "/kaggle/input/datasets/orvile/octdl-optical-coherence-tomography-dataset/OCTDL" 
+    PT_datasets_dir = "/kaggle/input/datasets/paultimothymooney/kermany2018" 
 else:
     PT_datasets_dir = 'deeplearning/data/datasets/'

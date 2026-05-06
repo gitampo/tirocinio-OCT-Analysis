737b01c12123615dc6af8fcce463a554c65dc05f
gitampo <tamponi.fra@libero.it>
2026-05-06 23:58:57 +0200

    fix kaggle

--- app/deeplearning/datasets/OCTDL.py
+++ app/deeplearning/datasets/OCTDL.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from configs.paths import PT_datasets_dir
 
-DATASET_NAME = 'OCT2017 '
+DATASET_NAME = 'OCT2017'
 
 labels = ['CNV', 'DME', 'DRUSEN', 'NORMAL']
 
@@ -12,6 +12,5 @@ def label2id(label):
     return labels.index(label)
 
 def get_patient_id(image_name):
-    # nel dataset Kaggle non esistono patient_id
-    # usiamo il nome immagine come identificativo univoco
-    return image_name
+    # usa il nome immagine come pseudo-patient-id
+    return image_name.split('-')[1]
